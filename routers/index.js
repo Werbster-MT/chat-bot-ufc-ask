d0a5fffd8312dbca43fc03a077d96e57ad58e9e4
Werbster <wmteixeira.88@gmail.com>
2025-07-12 21:20:45 -0300

    add user_perfil.

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,7 @@ const routerLogin = require("./loginRoute");
 const routerDashboard = require("./dashboardRoute");
 const componentesRoutes = require('./componentsRoute');
 const studentsRoutes = require('./studentsRoute');
+const usersRoutes = require('./usersRoute');
 
 
 module.exports = (app) => {
@@ -9,5 +10,6 @@ module.exports = (app) => {
     app.use(routerDashboard);
     app.use('/components', componentesRoutes);
     app.use(studentsRoutes);
+    app.use(usersRoutes);
     // adicione outros routers aqui conforme forem criados
 };
