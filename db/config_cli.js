26229ded682b6723ad20574974f8f1ed0daa9c23
Werbster <wmteixeira.88@gmail.com>
2025-07-23 19:48:26 -0300

    updating sequelize config.

--- db/config_cli.js
+++ db/config_cli.js
@@ -10,4 +10,13 @@ module.exports = {
     dialect: 'mysql',
     logging: false,
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+  },
 };
